9d9ebce9d76d4cedf7aa05036622bb5004a67055
alegalus <alejandrogalus83@yahoo.com.ar>
2021-08-23 23:22:25 -0300

    primer commit

---
+++ App.js
@@ -0,0 +1,35 @@
+mapboxgl.accessToken =
+  "pk.eyJ1IjoiYWxlZ2FsdXMiLCJhIjoiY2tzcDg5cW1pMDAyODJvcG9qd25kc3lydiJ9.z693bfaqubu2FUBdPkQG1g";
+
+navigator.geolocation.getCurrentPosition(posicionExitosa, posicionError, {
+  enableHighAccuracy: true,
+});
+
+function posicionExitosa(posicion) {
+  console.log(posicion);
+  centradoMapa([posicion.coords.longitude, posicion.coords.latitude]);
+}
+
+function posicionError() {}
+
+function centradoMapa(center) {
+  const map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: center,
+    zoom: 10,
+  });
+
+  let nav = new mapboxgl.NavigationControl()
+  map.addControl(nav);
+
+  let directions = new MapboxDirections({
+    accessToken: mapboxgl.accessToken
+    });
+    
+    map.addControl(directions, 'top-left')
+
+}
+
+
+
